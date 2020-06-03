b663370216eec5074260719390d84a2f76b652e2
JeongChanDo <djc4223@naver.com>
2020-06-04 02:44:25 +0900

    fix

--- chap2/drawSpacecraft.m
+++ chap2/drawSpacecraft.m
@@ -1,4 +1,4 @@
-function drawAircraftt(uu)
+function drawSpacecraft(uu)
 
     % process inputs to function
     pn       = uu(1);       % inertial North position     
@@ -16,7 +16,7 @@ function drawAircraftt(uu)
     t        = uu(13);       % time
 
     % define persistent variables 
-    persistent aircraft_handle;
+    persistent spacecraft_handle;
     persistent Vertices
     persistent Faces
     persistent facecolors
@@ -24,52 +24,50 @@ function drawAircraftt(uu)
     % first time function is called, initialize plot and persistent vars
     if t==0
         figure(1), clf
-        [Vertices, Faces, facecolors] = defineAircraft;
-        aircraft_handle = drawAircraftBody(Vertices,Faces,facecolors,...
+        [Vertices, Faces, facecolors] = defineSpacecraftBody;
+        spacecraft_handle = drawSpacecraftBody(Vertices,Faces,facecolors,...
                                                pn,pe,pd,phi,theta,psi,...
                                                [],'normal');
-        title('Aircraft')
+        title('Spacecraft')
         xlabel('East')
         ylabel('North')
         zlabel('-Down')
         view(32,47)  % set the vieew angle for figure
-        axis([-500,500,-500,500,-500,500]);
+        axis([-10,10,-10,10,-10,10]);
         hold on
         
     % at every other time step, redraw base and rod
     else 
-        drawAircraftBody(Vertices,Faces,facecolors,...
+        drawSpacecraftBody(Vertices,Faces,facecolors,...
                            pn,pe,pd,phi,theta,psi,...
-                           aircraft_handle);
+                           spacecraft_handle);
     end
 end
 
   
 %=======================================================================
-% drawAircraft
+% drawSpacecraft
 % return handle if 3rd argument is empty, otherwise use 3rd arg as handle
 %=======================================================================
 %
-function handle = drawAircraftBody(V,F,patchcolors,...
+function handle = drawSpacecraftBody(V,F,patchcolors,...
                                      pn,pe,pd,phi,theta,psi,...
                                      handle,mode)
-
-  V = rotate(V', phi, theta, psi);  % rotate Aircraft
-
-  V = translate(V, pn, pe, pd);  % translate Aircraft
+  V = rotate(V', phi, theta, psi)';  % rotate spacecraft
+  V = translate(V', pn, pe, pd)';  % translate spacecraft
   % transform vertices from NED to XYZ (for matlab rendering)
   R = [...
       0, 1, 0;...
       1, 0, 0;...
       0, 0, -1;...
       ];
-  V = V'*R;
+  V = V*R;
+  
   if isempty(handle)
   handle = patch('Vertices', V, 'Faces', F,...
                  'FaceVertexCData',patchcolors,...
                  'FaceColor','flat',...
                  'EraseMode', mode);
-             grido
   else
     set(handle,'Vertices',V,'Faces',F);
     drawnow
@@ -103,63 +101,48 @@ function XYZ = translate(XYZ,pn,pe,pd)
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% define aircraft vertices and faces
-function [V,F,facecolors] = defineAircraft()
-
-% Define the vertices (physical location of vertices
-V = [...
-    75, 0, 0;...       % pt 1
-    50, 25, -25;...    % pt 2
-    50, -25, -25;...   % pt 3
-    50, -25, 25;...    % pt 4
-    50, 25, 25;...     % pt 5
-    -225, 0, 0;...      % pt 6
-    0, 125, 0;...       % pt 7
-    -50, 125, 0;...      % pt 8
-    -50, -125, 0;...     % pt 9
-    0, -125, 0;...      % pt 10
-    -187.5, 75, 0;...   % pt 11
-    -225, 75, 0;...    % pt 12
-    -225, -75, 0;...   % pt 13
-    -187.5, -75, 0;...  % pt 14
-    -187.5, 0, 0;...     % pt 15
-    -225, 0, -62.5;...  % pt 16
+% define spacecraft vertices and faces
+function [V,F,colors] = defineSpacecraftBody()
+    % Define the vertices (physical location of vertices
+    V = [...
+        1    1    0;... % point 1
+        1   -1    0;... % point 2
+        -1   -1    0;... % point 3
+        -1    1    0;... % point 4
+        1    1   -2;... % point 5
+        1   -1   -2;... % point 6
+        -1   -1   -2;... % point 7
+        -1    1   -2;... % point 8
+        1.5  1.5  0;... % point 9
+        1.5 -1.5  0;... % point 10
+        -1.5 -1.5  0;... % point 11
+        -1.5  1.5  0;... % point 12
     ];
 
-% define faces as a list of vertices numbered above
-  F = [...
-        1, 1, 2, 5;...  % nose right
-        1, 1, 3, 4;...  % nose left
-        1, 1, 2, 3;...  % nose upper
-        1, 1, 4, 5;...  % nose bottom
-        2, 5, 6, 6;...  % fuselage right
-        3, 4, 6, 6;...  % fuselage left
-        2, 3, 6, 6;...  % fuselage upper
-        4, 5, 6, 6;...  % fuselage bottom
-        7, 8, 9, 10;...  % wing
-        11, 12, 13, 14;...  % horizontal tail
-        6, 15, 16, 6;...  % vertical tail
+    % define faces as a list of vertices numbered above
+    F = [...
+        1, 2,  6,  5;...  % front
+        4, 3,  7,  8;...  % back
+        1, 5,  8,  4;...  % right 
+        2, 6,  7,  3;...  % left
+        5, 6,  7,  8;...  % top
+        9, 10, 11, 12;... % bottom
         ];
 
-% define colors for each face    
-  myred = [1, 0, 0];
-  mygreen = [0, 1, 0];
-  myblue = [0, 0, 1];
-  myyellow = [1, 1, 0];
-  mycyan = [0, 1, 1];
+    % define colors for each face    
+    myred = [1, 0, 0];
+    mygreen = [0, 1, 0];
+    myblue = [0, 0, 1];
+    myyellow = [1, 1, 0];
+    mycyan = [0, 1, 1];
 
-  facecolors = [...
-    myyellow;...    % nose right
-    myyellow;...    % nose left
-    myyellow;...    % nose upper
-    myyellow;...    % nose bottom
-    myblue;...      % fuselage right
-    myblue;...      % fuselage left
-    myblue;...      % fuselage upper
-    myred;...      % fuselage bottom
-    mygreen;...     % wing
-    mygreen;...     % horizontal tail
-    myblue;...      % vertical tail
-    ];
+    colors = [...
+        myred;...    % front
+        mygreen;...  % back
+        myblue;...   % right
+        myyellow;... % left
+        mycyan;...   % top
+        mycyan;...   % bottom
+        ];
 end
   
